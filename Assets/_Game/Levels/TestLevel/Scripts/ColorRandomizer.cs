35cc2986ab6c74bce3a4a943a2daa6eee8da7251
Andrey Torchinskiy <andrey@voodoo.io>
2023-02-01 16:28:35 +0100

    new enemies, DoTween Pro

--- Assets/_Game/Levels/TestLevel/Scripts/ColorRandomizer.cs
+++ Assets/_Game/Levels/TestLevel/Scripts/ColorRandomizer.cs
@@ -1,18 +1,33 @@
 using UnityEngine;
+using Sirenix;
+using Sirenix.OdinInspector;
 
 public class ColorRandomizer : MonoBehaviour
 {
-    [SerializeField] private Vector3 _startRGBValues = Vector3.zero;
-    [SerializeField] private Vector3 _endRGBValues = Vector3.one;
-    private static readonly int BaseColor = Shader.PropertyToID("_BaseColor");
+	[SerializeField] private bool _uniform = false;
+	[HideIf("_uniform")][SerializeField] private Vector3 _startRGBValues = Vector3.zero;
+	[HideIf("_uniform")] [SerializeField] private Vector3 _endRGBValues = Vector3.one;
+	[ShowIf("_uniform")] [SerializeField] private float _startValue = 0f;
+	[ShowIf("_uniform")] [SerializeField] private float _endValue = 1f;
+	private static readonly int BaseColor = Shader.PropertyToID("_BaseColor");
 
     private void OnEnable()
     {
         Renderer rend = GetComponent<Renderer>();
-        Color color = new Color(Random.Range(_startRGBValues.x, _endRGBValues.x),
+		Color color;
+		if (_uniform)
+		{
+			float random = Random.Range(_startValue, _endValue);
+			color = new Color(random, random, random, 1f);
+		}
+		else
+		{
+			color = new Color(Random.Range(_startRGBValues.x, _endRGBValues.x),
 								Random.Range(_startRGBValues.y, _endRGBValues.y),
 								Random.Range(_startRGBValues.z, _endRGBValues.z),
-                                1f);
+								1f);
+		}
+        
 
         MaterialPropertyBlock block = new MaterialPropertyBlock();
         rend.GetPropertyBlock(block);
